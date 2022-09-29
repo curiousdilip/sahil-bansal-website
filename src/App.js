da7bc12eea099b55bc90247fd6cb420e3e1836d8
Dilip Kumar <curiousdilip@gmail.com>
2022-09-29 16:37:20 +0530

    issue fixed

--- src/App.js
+++ src/App.js
@@ -1,60 +1,58 @@
-import React from "react";
-// import {Spline} from "@splinetool/react-spline"
-import { useState } from "react";  
-import { IoMenu,IoLogoGithub } from "react-icons/io5";
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import  Spline  from "@splinetool/react-spline";
+import Sahil from "./img/sahil.jpg";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 import { Experience, Projects, SocialLinks } from "./data";
-import sahilImg from "./img/sahil.jpg"
-import BgImg from "./img/bgimg.jpg"
-import { AnimatePresence, motion } from "framer-motion"
-
+import { IoLogoGithub, IoMenu } from "react-icons/io5";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
   const [isActive, setIsActive] = useState(false);
   return (
     <AnimatePresence initial={false}>
-    <div className="flex w-screen h-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
-      {/* -------------------------------- Navbar ---------------------------------- */}
+      <div className="flex w-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
+        <nav className="w-full px-6 z-50  fixed inset-x-0 top-2 flex justify-center items-center ">
+          <div className=" w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center">
+            <p className="text-lg text-slate-200 font-medium">Vetrivel Ravi</p>
 
-      <nav className="w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center">
-        <div className=" w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center">
-          <p className="text-lg text-slate-200 font-medium">Sahil Bansal</p>
+            <div className="hidden md:flex items-center gap-6 ml-6 flex-1">
+              <a
+                href="#home"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              >
+                Home
+              </a>
+              <a
+                href="#about"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              >
+                About
+              </a>
+              <a
+                href="#projects"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              >
+                Projects
+              </a>
+              <a
+                href="#contacts"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              >
+                Contact
+              </a>
+              <a
+                href="#"
+                className="ml-auto text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in"
+              >
+                Download
+              </a>
+            </div>
 
-          <div className="hidden md:flex items-center gap-6 ml-6 flex-1">
-            <a
-              href="#home"
-              className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
-            >
-              Home
-            </a>
-            <a
-              href="#about"
-              className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
-            >
-              About
-            </a>
-            <a
-              href="#projects"
-              className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
-            >
-              Projects
-            </a>
-            <a
-              href="#contacts"
-              className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
-            >
-              Contact
-            </a>
-            <motion.a whileTap={{scale:0.8}}
-              href="#"
-              className="ml-auto text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in"
-            >
-              Download
-            </motion.a>
-          </div>
-
-          <motion.div
+            <motion.div
               whileTap={{ scale: 0.6 }}
               className="block md:hidden  ml-auto cursor-pointer"
               onClick={() => setIsActive(!isActive)}
@@ -106,39 +104,32 @@ function App() {
                 </a>
               </motion.div>
             )}
-        </div>
-      </nav>
-      <div className="relative" id="home">
-        {/* <Spline scene="https://prod.spline.design/7S3xzYChFhqkgUan/scene.splinecode" /> */}
-        <img src={BgImg} alt="" />
+          </div>
+        </nav>
 
-        <div className="absolute bottom-10 w-full flex justify-center items-center">
+        <div className="relative" id="home">
+          <Spline scene="https://prod.spline.design/oo6IxFu8UDjFUmjD/scene.spline" />
+          <div className="absolute bottom-10 w-full flex justify-center items-center">
             <div className="shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl ">
               <p className="text-white">Press and drag to orbit</p>
             </div>
           </div>
-      </div>
+        </div>
 
-      {/*----------------------------- main sections ---------------------------*/}
-
-      <main className="w-[80%] mt-5">
-
-        {/*------------------------ about section ------------------------ */}
-        <section
+        <main className="w-[80%] mt-4">
+          <section
             className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24"
             id="about"
           >
-            {/* image-box */}
             <div className="w-full h-420 flex items-center justify-center">
               <div className="w-275 h-340 relative bg-emerald-200 rounded-md">
                 <img
-                  src={sahilImg}
+                  src={Sahil}
                   alt=""
                   className="w-full h-full absolute -right-4 top-4 object-cover rounded-lg drop-shadow-2xl"
                 />
               </div>
             </div>
-            {/* content-box */}
             <div className="w-full h-420 flex flex-col items-center justify-center ">
               <p className="text-lg text-textBase text-center">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -151,14 +142,14 @@ function App() {
                 vero nobis assumenda commodi magni.
               </p>
 
-              <motion.button whileTap={{scale:0.6}} class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
+              <button class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
                 <span class="w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                   Download
                 </span>
-              </motion.button>
+              </button>
             </div>
           </section>
-        {/*------------------------ timeline section ------------------------ */}
+
           <section className="w-full flex items-center justify-center">
             <VerticalTimeline>
               {Experience &&
@@ -189,14 +180,13 @@ function App() {
             </VerticalTimeline>
           </section>
 
-
           <section
             className="flex flex-wrap items-center justify-evenly my-24 gap-4"
             id="projects"
           >
-          {Projects &&
+            {Projects &&
               Projects.map((n, i) => (
-                <div
+                <motion.div
                   key={n.id}
                   className="border border-zinc-800 rounded-md p-2 min-w-[275px] md:max-w-[275px] hover:border-zinc-600 duration-100 ease-in-out"
                 >
@@ -217,15 +207,16 @@ function App() {
                         {n.techs}
                       </span>
                     </p>
-                    <motion.a whileTap={{scale:0.6}}  href={n.github}>
-                      <div whileTap={{ scale: 0.5 }}>
+                    <a href={n.github}>
+                      <motion.div whileTap={{ scale: 0.5 }}>
                         <IoLogoGithub className="text-textBase text-3xl cursor-pointer" />
-                      </div>
-                    </motion.a>
+                      </motion.div>
+                    </a>
                   </div>
-                </div>
+                </motion.div>
               ))}
           </section>
+
           <section
             id="contacts"
             className="flex flex-col items-center justify-evenly w-full my-24"
@@ -246,8 +237,8 @@ function App() {
                 ))}
             </div>
           </section>
-      </main>
-    </div>
+        </main>
+      </div>
     </AnimatePresence>
   );
 }
