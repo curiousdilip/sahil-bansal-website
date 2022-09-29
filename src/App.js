376b3831045b89e078f1b6ac621e4a360515c378
Dilip Kumar <curiousdilip@gmail.com>
2022-09-29 14:08:20 +0530

    about section done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [isActive, setIsActive] = useState(false);
   return (
     <div className="flex w-screen h-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
-      {/* Navbar */}
+      {/* -------------------------------- Navbar ---------------------------------- */}
 
       <nav className="w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center">
         <div className=" w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center">
@@ -111,6 +111,47 @@ function App() {
             </div>
           </div>
       </div>
+
+      {/*----------------------------- main sections ---------------------------*/}
+
+      <main className="w-[80%] mt-5">
+
+        {/*------------------------ about section ------------------------ */}
+        <section
+            className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24"
+            id="about"
+          >
+            {/* image-box */}
+            <div className="w-full h-420 flex items-center justify-center">
+              <div className="w-275 h-340 relative bg-emerald-200 rounded-md">
+                <img
+                  src="https://source.unsplash.com/random/?photo/"
+                  alt=""
+                  className="w-full h-full absolute -right-4 top-4 object-cover rounded-lg drop-shadow-2xl"
+                />
+              </div>
+            </div>
+            {/* content-box */}
+            <div className="w-full h-420 flex flex-col items-center justify-center ">
+              <p className="text-lg text-textBase text-center">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Sapiente dicta recusandae commodi praesentium officia, aliquam
+                cumque voluptatem dolor enim ducimus id maiores consequatur
+                dolorem! Architecto aperiam corrupti nostrum eum similique!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis saepe dolor illo. Tenetur labore impedit nihil natus
+                explicabo perspiciatis nisi autem aut numquam dolorum, voluptas
+                vero nobis assumenda commodi magni.
+              </p>
+
+              <button class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
+                <span class="w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  Download
+                </span>
+              </button>
+            </div>
+          </section>
+      </main>
     </div>
   );
 }
