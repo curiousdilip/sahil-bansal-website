e5abaa1f87df2c79c617d5eef563ff18ca5edfad
Dilip Kumar <curiousdilip@gmail.com>
2022-09-29 16:58:39 +0530

    design fixed

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <div className="flex w-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
         <nav className="w-full px-6 z-50  fixed inset-x-0 top-2 flex justify-center items-center ">
           <div className=" w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center">
-            <p className="text-lg text-slate-200 font-medium">Vetrivel Ravi</p>
+            <p className="text-lg text-slate-200 font-medium">Sahil Bansal</p>
 
             <div className="hidden md:flex items-center gap-6 ml-6 flex-1">
               <a
@@ -45,7 +45,7 @@ function App() {
                 Contact
               </a>
               <a
-                href="#"
+                href="#contact"
                 className="ml-auto text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in"
               >
                 Download
@@ -96,7 +96,7 @@ function App() {
                   Contact
                 </a>
                 <a
-                  href="#"
+                  href="#contact"
                   className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in"
                   onClick={() => setIsActive(false)}
                 >
@@ -108,7 +108,7 @@ function App() {
         </nav>
 
         <div className="relative" id="home">
-          <Spline scene="https://prod.spline.design/oo6IxFu8UDjFUmjD/scene.spline" />
+        <Spline scene="https://prod.spline.design/oo6IxFu8UDjFUmjD/scene.spline" />
           <div className="absolute bottom-10 w-full flex justify-center items-center">
             <div className="shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl ">
               <p className="text-white">Press and drag to orbit</p>
--- src/data.js
+++ src/data.js
@@ -2,16 +2,50 @@ import { IoCodeWorking, IoLogoGithub, IoLogoLinkedin, IoLogoTwitter, IoMail } fr
 import BgImg from "./img/bgimg.jpg"
 
 export const Experience=[
-    {id:1,
-    date:"2016-presnt",
-iconsSrc:<IoCodeWorking/>,
-title:"delhi,Qatar",
-description:"lorem ajkshdhas ahsfkjhkjsadfh kjsadhgkjshdg h kjsdghs ldkjgh hkjasfdkjh kjhkaljshd flhakfljh ahfkj h",},
-{id:2,
-    date:"2016-presnt",
-iconsSrc:<IoCodeWorking/>,
-title:"delhi,Qatar",
-description:"lorem ajkshdhas ahsfkjhkjsadfh kjsadhgkjshdg h kjsdghs ldkjgh hkjasfdkjh kjhkaljshd flhakfljh ahfkj h",}
+ {    id: 1,
+    date: "2016 - present",
+    iconsSrc: <IoCodeWorking />,
+    title: "Creative Director",
+    location: "Doha, Qatar",
+    description:
+      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+  },
+  {
+    id: 2,
+    date: "2014 - 2016",
+    iconsSrc: <IoCodeWorking />,
+    title: "Creative Director",
+    location: "Doha, Qatar",
+    description:
+      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+  },
+  {
+    id: 3,
+    date: "2012 - 2014",
+    iconsSrc: <IoCodeWorking />,
+    title: "Creative Director",
+    location: "Doha, Qatar",
+    description:
+      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+  },
+  {
+    id: 4,
+    date: "2011 - 2012",
+    iconsSrc: <IoCodeWorking />,
+    title: "Creative Director",
+    location: "Doha, Qatar",
+    description:
+      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+  },
+  {
+    id: 5,
+    date: "2010 - 2011",
+    iconsSrc: <IoCodeWorking />,
+    title: "Creative Director",
+    location: "Doha, Qatar",
+    description:
+      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+  },
 
 ]
 
